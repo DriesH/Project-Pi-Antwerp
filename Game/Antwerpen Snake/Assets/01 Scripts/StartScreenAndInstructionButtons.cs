9f2586f5de666be2436d47f9bf168da2939827ae
DriesH <dries.heyninck@gmail.com>
2016-05-17 15:12:53 +0200

    fix

--- Game/Antwerpen Snake/Assets/01 Scripts/StartScreenAndInstructionButtons.cs
+++ Game/Antwerpen Snake/Assets/01 Scripts/StartScreenAndInstructionButtons.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class StartScreenAndInstructionButtons : MonoBehaviour {
-  //class that controls the UI buttons of the main menu of the game, the instructionbuttons and the buttons of the list of projects
 
   public void StartGame() //start the game
   {
@@ -18,11 +17,5 @@ public class StartScreenAndInstructionButtons : MonoBehaviour {
   public void GoToStartScreenGame() //go to the main menu of the game
   {
     Application.LoadLevel("MainScreenSnake");
-  }
-
-  //nog wegdoen = tijdelijk ==> moet met database in panelproject opgeroepen kunnen wordne
-  public void TijdelijkNaarHome()
-  {
-    Application.OpenURL("http://pi.multimediatechnology.be/");
   }
 }
