29fca2d0c27f882174c292323498307f43f3da71
Dries Heyninck <dries.heyninck@gmail.com>
2016-05-24 21:57:35 +0200

    Fix layout

--- Website/antwerpen-website/resources/views/admin/appvraag-verwijderen.blade.php
+++ Website/antwerpen-website/resources/views/admin/appvraag-verwijderen.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
 <div class="container">
+    <a href="{{ URL::previous() }}" class="btn btn-primary"><i class="fa fa-arrow-left"></i>Terug</a>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h3>Bent u zeker dat u deze vraag van project: {{$project->naam}} wilt verwijderen?</h3>
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <h3>Bent u zeker dat u deze vraag van project: <em>{{$project->naam}}</em> wilt verwijderen?</h3>
                 </div>
                 <div class="panel-body">
-                      <div class="bs-callout bs-callout-primary">
-                        <h4>{{$appquestion->question}}</h4>
-                    {{ Form::open(array(
-                      'url' => Request::fullUrl(),
-                      'class' => 'form-horizontal',
-                      'role' => 'form')) }}
-                    <button type="submit" class="btn btn-danger"><i class="fa fa-edit"></i>Appvraag verwijderen</button>
-                    <a class="btn btn-default" href="/admin/project-bewerken/{{$project->idProject}}/appvragen" role="button"><i class="fa fa-edit"></i>Annuleren</a>
-                       {{ Form::close() }}
+                      <div class="bs-callout bs-callout-danger">
+                        <h3>{{$appquestion->question}}</h3>
+                        {{ Form::open(array(
+                          'url' => Request::fullUrl(),
+                          'class' => 'form-horizontal',
+                          'role' => 'form')) }}
+                            <a class="btn btn-warning" href="/admin/project-bewerken/{{$project->idProject}}/appvragen" role="button"><i class="fa fa-ban"></i>Annuleren</a>
+                            <button type="submit" class="btn btn-danger pull-right"><i class="fa fa-trash"></i>Appvraag verwijderen</button>
+                            
+                        {{ Form::close() }}
                     </div>
                 </div>
             </div>
