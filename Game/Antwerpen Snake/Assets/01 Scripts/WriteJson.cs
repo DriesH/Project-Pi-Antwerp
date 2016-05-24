c944268ae1b7aa96a50b612c26abbd03e87dbd81
Jiska Baeten <jiska.baeten@student.kdg.be>
2016-05-24 15:16:56 +0200

    mogelijk werkende code voor POST server

--- Game/Antwerpen Snake/Assets/01 Scripts/WriteJson.cs
+++ Game/Antwerpen Snake/Assets/01 Scripts/WriteJson.cs
@@ -1,22 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
-using LitJson;
+//using LitJson;
 
 public class WriteJson : MonoBehaviour {
   
-  private string qData = "Test";
-  JsonData answerData;
+  private string writeURL = "";
+  private Dictionary<string, string> answers = new Dictionary<string, string>();
 
 	void Start () {
-  //  answerData = JsonMapper.ToJson(qData);
-   // File.WriteAllText(Application.dataPath + "/06 Resources/writeJson.json", answerData.ToString());
+    answers.Add("Question1", "AnswerA");
 
-  //  WWWForm form = new WWWForm();
-
-
-      //postdata: A byte array of data to be posted to the url.
-      //headers 	A dictionary that contains the header keys and values to pass to the server.
+    StartCoroutine(writeToFile());
 	}
 
+  IEnumerator writeToFile () { //Ienum wacht op de yield, wa hier gwn de data ophalen is (is standaard)
+    WWWForm form = new WWWForm();
+    foreach (KeyValuePair<string, string> post_arg in answers)
+    {
+      form.AddField(post_arg.Key, post_arg.Value); //stopt dictionary in een form
+    }
+    WWW www = new WWW(writeURL, form); //stuurt form naar URL
+    yield return www;
+
+ 
+
+    File.WriteAllText(Application.dataPath + "/06 Resources/writeJson.json", www.text);
+    File.WriteAllText(Application.dataPath + "/06 Resources/writeJson.json", form.data[0].ToString());
+    Debug.Log("File written");
+  }
+
 }
+
+//postdata: A byte array of data to be posted to the url. => zelfde data als form
+//headers 	A dictionary that contains the header keys and values to pass to the server.s
+//  vb van url voor POSTEN : http://pi.multimediatechnology.be/API/post?antwoord=Juist&?user=1?&project=1
